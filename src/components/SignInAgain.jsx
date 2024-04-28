f2a853fb071aa384e5153a611c4a8cbc0434130b
kunchamadhu <km922047@student.nitw.ac.in>
2024-04-29 03:11:58 +0530

    finalchanges2 29 4

---
+++ src/components/SignInAgain.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+
+const SignInAgain = () => {
+  const styles = {
+    body: {
+        fontFamily: 'Arial, sans-serif',
+        margin: 0,
+        padding: 0
+      },
+    h3: {
+      color: '#333',
+      textAlign: 'center',
+      marginTop: '50px'
+    },
+    p: {
+      textAlign: 'center',
+      marginTop: '20px'
+    },
+    a: {
+      color: '#fff',
+      backgroundColor: '#007bff',
+      padding: '10px 20px',
+      textDecoration: 'none',
+      borderRadius: '5px',
+      transition: 'background-color 0.3s ease'
+    },
+    aHover: { // Note: This won't work directly in inline styles
+      backgroundColor: '#0056b3'
+    }
+  };
+
+  return (
+    <div style={styles.body}>
+      <h3 style={styles.h3}>Welcome to Skilled Tailor</h3>
+      <p style={styles.p}>
+        <a style={styles.a} href="https://kmadhukuncha-auth.auth.us-east-1.amazoncognito.com/logout?client_id=5mpkb6pvjt1csdl9rk4ctg9ded&logout_uri=https://skilledtailor.site/signin">
+          Please Sign In Again
+        </a>
+      </p>
+    </div>
+  );
+};
+
+export default SignInAgain;
